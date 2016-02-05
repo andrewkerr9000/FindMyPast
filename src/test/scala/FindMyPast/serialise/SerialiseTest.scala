49423b69a513312225a343d38355b5ed705d3dc9
andrew <andrewkerr9000@gmail.com>
2016-02-05 11:09:54 +0000

    failing: serialise 1x1 table of 1

--- src/test/scala/FindMyPast/serialise/SerialiseTest.scala
+++ src/test/scala/FindMyPast/serialise/SerialiseTest.scala
@@ -5,8 +5,14 @@ import FindMyPast.MySpec
 class SerialiseTest extends MySpec {
 
   "Serialiser" should {
-    "return empty string if empty Seq passed in" in {
+    "return empty string if empty table passed in" in {
       Serialise(Nil) should be("")
+    }
+
+    "serialise a 1 x 1 table" in {
+      Serialise(Seq(Seq(1))) should be(
+        """#  | 1
+           #1 | 1""".stripMargin('#'))
     }
   }
 }
