2421c1495571ff526256f7244083533b3da69bcf
andrew <andrewkerr9000@gmail.com>
2016-02-05 10:27:36 +0000

    minor refactoring and commenting

--- src/main/scala/FindMyPast/prime/FirstNPrimesGenerator.scala
+++ src/main/scala/FindMyPast/prime/FirstNPrimesGenerator.scala
@@ -13,19 +13,25 @@ object FirstNPrimesGenerator {
   // Requires infinite lazy lists - Stream in Scala
   def apply(n: Int): Seq[Long] = {
     require(n >= 0)
-    // Sometimes an early return can make the rest of a method simpler. I don't like returning in the middle of a method
+    // Sometimes an early return can make the rest of a method simpler.
+    // I don't like returning in the middle of a method though. It's unclear.
     if(n == 0) return Nil
 
-    def knownMultiplesOf(n: Long) = Stream.iterate(start = n)(_ + n)
+    // Sieve of Eratosthenes marks off multiples of each prime as non-prime up to a certain number.
+    // By using lazy infinite lists we can mark off all multiples, and then check them when needed (and only once).
+    def multiplesOf(n: Long) = {
+      val firstMultiple = n * 2
+      Stream.iterate(start = firstMultiple)(_ + n) // shorthand for i = i + n
+    }
 
     // storing the length so it doesn't need to be recalculated every time - which is O(n) on most Scala collections
     @tailrec // not required for tail call optimisation, but makes the compiler moan if it is not possible
-    def recur(candidate: Long, length: Int, knownPrimes: Seq[Long], knownNonPrimes: Seq[Stream[Long]]): Seq[Long] = {
+    def constructPrimesFrom(candidate: Long, length: Int, knownPrimes: Seq[Long], knownNonPrimes: Seq[Stream[Long]]): Seq[Long] = {
       @tailrec
       def notContained(candidate: Long, stream: Stream[Long]): Boolean = {
         if (candidate == stream.head) {
           false
-        } else if (stream.head > candidate) {
+        } else if (stream.head > candidate) { // as stream is increasing we know we'll never get a match now
           true
         } else {
           notContained(candidate, stream.tail)
@@ -35,16 +41,17 @@ object FirstNPrimesGenerator {
       if(length == n) {
         knownPrimes
       } else {
+        // if the next number hasn't already been found to be a multiple of any smaller primes the it itself is prime
         if(knownNonPrimes.forall(notContained(candidate, _))) {
-          recur(candidate + 1, length + 1, knownPrimes :+ candidate, knownNonPrimes :+ knownMultiplesOf(candidate))
+          constructPrimesFrom(candidate + 1, length + 1, knownPrimes :+ candidate, knownNonPrimes :+ multiplesOf(candidate))
         } else {
-          recur(candidate + 1, length, knownPrimes, knownNonPrimes)
+          constructPrimesFrom(candidate + 1, length, knownPrimes, knownNonPrimes)
         }
       }
     }
 
     val firstPrime = 2
-    recur(candidate = firstPrime + 1, length = 1, knownPrimes = Seq(firstPrime), knownNonPrimes = Seq(knownMultiplesOf(firstPrime)))
+    constructPrimesFrom(candidate = firstPrime + 1, length = 1, knownPrimes = Seq(firstPrime), knownNonPrimes = Seq(multiplesOf(firstPrime)))
   }
 
 }
