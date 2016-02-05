21a03c54e7f1cd23d3c6cef27dc15b36928baf98
andrew <andrewkerr9000@gmail.com>
2016-02-05 12:58:32 +0000

    passing: test on App that combines components, mocking Printer

--- src/main/scala/FindMyPast/app/App.scala
+++ src/main/scala/FindMyPast/app/App.scala
@@ -1,11 +1,19 @@
 package FindMyPast.app
 
 import FindMyPast.Printer
+import FindMyPast.prime.FirstNPrimesGenerator
+import FindMyPast.serialise.Serialise
+import FindMyPast.tabulate.Tabulate
 
 trait Config {
   val printer: Printer
 }
 
 class App(implicit val config: Config) {
-  def run(tableSize: Int): Unit = ???
+  def run(tableSize: Int): Unit = {
+    val primes = FirstNPrimesGenerator(tableSize)
+    val table = Tabulate(primes)
+    val output = Serialise(table)
+    config.printer.print(output)
+  }
 }
