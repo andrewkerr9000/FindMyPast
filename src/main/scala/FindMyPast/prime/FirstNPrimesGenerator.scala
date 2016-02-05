3fcbce752b01d7a37472b80893be9753aa38ea60
andrew <andrewkerr9000@gmail.com>
2016-02-05 08:19:24 +0000

    passing: negative number of primes test

--- src/main/scala/FindMyPast/prime/FirstNPrimesGenerator.scala
+++ src/main/scala/FindMyPast/prime/FirstNPrimesGenerator.scala
@@ -7,6 +7,7 @@ object FirstNPrimesGenerator {
   // The use of an apply method allows me to use the Object with function-like syntax
   // i.e. FirstNPrimes(3) as well as of FirstNPrimes.apply(3)
   def apply(n: Int): Seq[Int] = {
+    require(n >= 0)
     Seq.fill(n)(2)
   }
 
