03372925787b26b6017d1104107c25533aaac689
andrew <andrewkerr9000@gmail.com>
2016-02-05 10:57:57 +0000

    passing: spec for correctly tabulated data

--- src/main/scala/FindMyPast/tabulate/Tabulate.scala
+++ src/main/scala/FindMyPast/tabulate/Tabulate.scala
@@ -2,5 +2,10 @@ package FindMyPast.tabulate
 
 object Tabulate {
 
-  def apply(numbers: Seq[Int]): Seq[Seq[Int]] = Nil
+  def apply(numbers: Seq[Int]): Seq[Seq[Int]] = {
+    val size = numbers.length
+    Seq.tabulate(size, size) { (i, j) =>
+      numbers(i) * numbers(j)
+    }
+  }
 }
