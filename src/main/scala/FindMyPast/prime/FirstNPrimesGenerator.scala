39678742d12b7dc58e5e45af8bab63e197dde453
andrew <andrewkerr9000@gmail.com>
2016-02-05 07:47:21 +0000

    passing: asking for 0 primes

--- src/main/scala/FindMyPast/prime/FirstNPrimesGenerator.scala
+++ src/main/scala/FindMyPast/prime/FirstNPrimesGenerator.scala
@@ -7,6 +7,6 @@ object FirstNPrimesGenerator {
   // The use of an apply method allows me to use the Object with function-like syntax
   // i.e. FirstNPrimes(3) as well as of FirstNPrimes.apply(3)
   // ??? is a macro that compiles but throws a runtime exception, handy for not-yet-implemented code
-  def apply(n: Int): Seq[Int] = ???
+  def apply(n: Int): Seq[Int] = Nil
 
 }
